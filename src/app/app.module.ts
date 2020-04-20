fc8e3489a8083a7de79c74f009b24afd75fe7a14
Joa Moreno <tec.ignaciomoreno@gmail.com>
2020-04-19 22:00:53 -0300

    first encounter with NgRx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// NgRx
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
+// Reducers
+import { reducers, metaReducers } from './reducers/reducers';
+
+const NGRX_IMPORTS = [
+  StoreModule.forRoot(reducers, { metaReducers }),
+  StoreRouterConnectingModule.forRoot({stateKey: 'router'}),
+  EffectsModule.forRoot([]),
+  StoreDevtoolsModule.instrument({ name: "AngularChatNgRx", logOnly: environment.production })
+
+]
+
+// Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +28,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ...NGRX_IMPORTS
   ],
   providers: [],
   bootstrap: [AppComponent]
